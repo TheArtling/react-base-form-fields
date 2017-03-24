e65e7235fe3a06a86d1b0a68ff1f76e97bb78014
Nicholas Ng <nzy.nicholas@gmail.com>
2017-03-24 12:08:07 +0800

    Renamed RNBaseTextInput to BaseTextInput

--- src/RNBaseTextInput.jsx
+++ src/RNBaseTextInput.jsx
@@ -4,7 +4,7 @@ import { TextInput } from 'react-native'
 
 
 @Formfield
-export class RNBaseTextInput extends React.Component {
+export class BaseTextInput extends React.Component {
   // Base component for all Input elements of type `text`.
   // In order to style this, write a wrapper component that has this as a child
   // and passes in all necessary props.
